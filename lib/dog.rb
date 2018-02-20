fa94eb24af51d2260e12f4fd8786abc28e7762ef
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-20 23:40:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,6 @@ class Dog
 
 fido =  Dog.new 
 snoopy = Dog.new 
+lassie = Dog.new 
 
 
-
-class Dog
-end
- 
-fido = Dog.new
-fido #=> #<Dog:0x007fc52c2d7d20>
- 
-snoopy = Dog.new
-snoopy #=> #<Dog:0x007fc52c2d4170>
